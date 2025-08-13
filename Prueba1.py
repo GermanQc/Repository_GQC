a2c9c91ad4b47192274999be6b20b5ce0bc9dd75
Germán Quiñones <germanquinones@MacBook-Air-de-German.local>
2025-08-13 16:33:09 -0400

    commit3 git push

--- Prueba1.py
+++ Prueba1.py
@@ -1,12 +1,3 @@
-# Definimos la palabra
-palabra = "ejemplo"
+Variable_1 = "hello world"
+print(Variable_1)
 
-# Usamos la función len() para contar los caracteres de la palabra
-numero_de_letras = len(palabra)
-
-# Imprimimos el resultado
-print(f"La palabra '{palabra}' tiene {numero_de_letras} letras.")
-
-# --- Otro ejemplo ---
-otra_palabra = "computadora"
-print(f"La palabra '{otra_palabra}' tiene {len(otra_palabra)} letras.")
